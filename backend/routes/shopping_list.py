90ec9defef7b4916894f10eafc9ddc0c32e8a263
Kerzoken <kerzo.gm@gmail.com>
2024-06-24 23:34:04 +0300

    Added get requests

--- backend/routes/shopping_list.py
+++ backend/routes/shopping_list.py
@@ -4,6 +4,21 @@ from backend.models import db, ShoppingList, Product
 shopping_list_bp = Blueprint('shopping_list', __name__)
 
 
+@shopping_list_bp.route('/api/shopping-lists/', methods=['GET'])
+def get_all_shopping_lists():
+    shopping_lists = ShoppingList.query.all()
+    return jsonify([shopping_list.to_dict() for shopping_list in shopping_lists])
+
+
+@shopping_list_bp.route('/api/shopping-lists/<int:list_id>', methods=['GET'])
+def get_shopping_list(list_id):
+    shopping_list = ShoppingList.query.get(list_id)
+    if shopping_list:
+        return jsonify(shopping_list.to_dict())
+    else:
+        return jsonify({'error': 'Shopping list not found'}), 404
+
+
 @shopping_list_bp.route('/api/shopping-lists/', methods=['POST'])
 def create_shopping_list():
     new_list = ShoppingList()
@@ -12,7 +27,7 @@ def create_shopping_list():
     return jsonify(new_list.to_dict()), 201
 
 
-@shopping_list_bp.route('/api/shopping-lists/<int:list_id>', methods=['PUT'])
+@shopping_list_bp.route('/api/shopping-lists/<int:list_id>', methods=['POST, PUT'])
 def add_products_to_shopping_list(list_id):
     shopping_list = ShoppingList.query.get(list_id)
     if shopping_list:
@@ -21,5 +36,16 @@ def add_products_to_shopping_list(list_id):
         shopping_list.products.extend(products)
         db.session.commit()
         return jsonify(shopping_list.to_dict())
+    else:
+        return jsonify({'error': 'Shopping list not found'}), 404
+
+
+@shopping_list_bp.route('/api/shopping-lists/<int:list_id>', methods=['DELETE'])
+def delete_shopping_list(list_id):
+    shopping_list = ShoppingList.query.get(list_id)
+    if shopping_list:
+        db.session.delete(shopping_list)
+        db.session.commit()
+        return jsonify({'message': 'Shopping list deleted'})
     else:
         return jsonify({'error': 'Shopping list not found'}), 404
