492049494d56c4024cc346db38ef545bc668cc75
denis.gm <denis.g.mirchev@gmail.com>
2024-06-25 16:12:55 +0300

    Added map generation

---
+++ backend/map_generation.py
@@ -0,0 +1,39 @@
+from backend.models import Location
+
+
+def generate_map():
+    width = 41
+    height = 21
+
+    map = [['.' for _ in range(width)] for _ in range(height)]
+
+    locations = Location.query.all()
+    for location in locations:
+        x = location.x
+        y = location.y
+
+        if (location.location_id == 'BL'):
+            map[y][x] = 'X'
+        elif (location.location_id.startswith('P')):
+            map[y][x] = 'P'
+        elif (location.location_id.startswith('CA')):
+            map[y][x] = 'C'
+        elif (location.location_id.startswith('S')):
+            map[y][x] = 'S'
+        elif (location.location_id == 'EN'):
+            map[y][x] = 'E'
+        elif (location.location_id == 'EX'):
+            map[y][x] = 'F'
+
+    return map
+
+
+if __name__ == '__main__':
+    generate_map()
+
+
+# map = generate_map()
+# for i in range(20, -1, -1):
+#     for j in range(41):
+#         print(map[i][j], end=' ')
+#         print()
