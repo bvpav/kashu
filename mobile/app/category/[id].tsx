dfbb170cd5ebaccf190c41262201c8ae1b317412
Dimitar Zhelev <dimitar.n.zhelev.2020@elsys-bg.org>
2024-06-25 15:12:34 +0300

    made the design more responsive

--- mobile/app/category/[id].tsx
+++ mobile/app/category/[id].tsx
@@ -6,6 +6,7 @@ import {
   StyleSheet,
   Pressable,
   ScrollView,
+  Dimensions,
   Image,
 } from "react-native";
 import { Stack, useLocalSearchParams } from "expo-router";
@@ -19,6 +20,8 @@ const CategoryDetails = () => {
   const id = Array.isArray(local.id)
     ? parseInt(local.id[0])
     : parseInt(local.id ?? "");
+
+  const screenWidth = Dimensions.get("window").width;
 
   const products = getCategoryProducts(id);
   const category_name = getCategoryNameById(id);
@@ -35,15 +38,10 @@ const CategoryDetails = () => {
           flexDirection: "row",
           flexWrap: "wrap",
           justifyContent: "space-evenly",
+          width: "100%",
         }}
       >
         {products.map((item) => (
-          // <Link
-          //   href={"category/" + item.id}
-          //   asChild
-          //   key={item.id}
-          //   className="w-[40%]"
-          // >
           <Pressable
             key={item.product_id}
             style={styles.categoryContainer}
@@ -52,14 +50,13 @@ const CategoryDetails = () => {
             <Image
               source={require(`@/assets/category/4.jpg`)}
               style={{
-                height: 140,
-                width: 140,
+                height: screenWidth * 0.35,
+                width: screenWidth * 0.35,
+                borderRadius: 8,
               }}
-              className="rounded-xl"
             />
-            <Text className="text-xl">{item.name}</Text>
+            <Text className="text-xl text-center">{item.name}</Text>
           </Pressable>
-          // </Link>
         ))}
       </ScrollView>
     </View>
@@ -69,8 +66,7 @@ const CategoryDetails = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    backgroundColor: "white",
   },
   title: {
     fontSize: 24,
@@ -80,6 +76,8 @@ const styles = StyleSheet.create({
     alignItems: "center",
     margin: 5,
     borderWidth: 1,
+    maxWidth: "40%",
+    textAlign: "center",
     borderColor: "grey",
     // iOS Shadow
     shadowColor: "#000",
--- mobile/app/category/index.tsx
+++ mobile/app/category/index.tsx
@@ -7,6 +7,7 @@ import {
   StyleSheet,
   Image,
   ScrollView,
+  Dimensions,
   Pressable,
 } from "react-native";
 
@@ -17,7 +18,7 @@ interface Category {
 
 export default function ProductsScreen() {
   const [categories, setCategories] = useState<Category[]>([]);
-
+  const screenWidth = Dimensions.get("window").width;
   useEffect(() => {
     setCategories(categoriesData.categories);
     console.log(categoriesData.categories);
@@ -34,20 +35,15 @@ export default function ProductsScreen() {
         }}
       >
         {categories.map((item) => (
-          <Link
-            href={"category/" + item.id}
-            asChild
-            key={item.id}
-            className="w-[40%]"
-          >
+          <Link href={"category/" + item.id} asChild key={item.id}>
             <Pressable style={styles.categoryContainer} className="w-full">
               <Image
                 source={require(`../../assets/category/4.jpg`)}
                 style={{
-                  height: 140,
-                  width: 140,
+                  height: screenWidth * 0.35,
+                  width: screenWidth * 0.35,
+                  borderRadius: 8,
                 }}
-                className="rounded-xl"
               />
               <Text className="text-xl">{item.name}</Text>
             </Pressable>
@@ -63,6 +59,8 @@ const styles = StyleSheet.create({
     alignItems: "center",
     margin: 5,
     borderWidth: 1,
+    maxWidth: "40%",
+    textAlign: "center",
     borderColor: "grey",
     // iOS Shadow
     shadowColor: "#000",
