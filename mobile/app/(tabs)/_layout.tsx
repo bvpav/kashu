a3d3e61755069a4281c5500acb5f43cdcea1b971
bvpav <78971592+bvpav@users.noreply.github.com>
2024-06-25 13:59:26 +0300

    setup formatting with prettier

--- mobile/app/(tabs)/_layout.tsx
+++ mobile/app/(tabs)/_layout.tsx
@@ -1,10 +1,11 @@
-import {Tabs} from "expo-router";
+import { Tabs } from "expo-router";
+import { Text } from "react-native";
 
 export default function TabsLayout() {
   return (
-      <Tabs screenOptions={{ tabBarActiveTintColor: 'blue' }}>
-        <Tabs.Screen name="index" />
-        <Tabs.Screen name="test" />
+    <Tabs screenOptions={{ tabBarActiveTintColor: "blue" }}>
+      <Tabs.Screen name="index" />
+      <Tabs.Screen name="test" />
     </Tabs>
   );
 }
--- mobile/app/(tabs)/index.tsx
+++ mobile/app/(tabs)/index.tsx
@@ -1,10 +1,12 @@
-import { Link, Stack } from "expo-router";
+import { Link, Tabs } from "expo-router";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function IndexScreen() {
   return (
     <>
-      <Stack.Screen options={{ title: "Home screen" }} />
+      <Tabs.Screen
+        options={{ title: "Home screen", tabBarStyle: { display: "none" } }}
+      />
       <View style={styles.container} className="bg-slate-100">
         <Text className="text-black">This screen is the index screen.</Text>
         <Link href="/(tabs)/test" className="bg-gray-400" style={styles.link}>
