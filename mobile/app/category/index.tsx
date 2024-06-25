c31ef1ee06d7b40201d1080d4d74e1e598778150
Dimitar Zhelev <dimitar.n.zhelev.2020@elsys-bg.org>
2024-06-25 16:29:06 +0300

    changed categories design

--- mobile/app/category/index.tsx
+++ mobile/app/category/index.tsx
@@ -1,5 +1,5 @@
 import { Link } from "expo-router";
-import categoriesData from "../categories.json"; // replace with the actual path to your JSON file
+import categoriesData from "../categories.json";
 import React, { useState, useEffect } from "react";
 import {
   View,
@@ -21,31 +21,67 @@ export default function ProductsScreen() {
   const screenWidth = Dimensions.get("window").width;
   useEffect(() => {
     setCategories(categoriesData.categories);
-    console.log(categoriesData.categories);
   }, []);
+
+  const styles = StyleSheet.create({
+    categoryContainer: {
+      height: screenWidth * 0.35,
+      flexDirection: "row",
+      alignItems: "center",
+      marginVertical: 4,
+      borderWidth: 1,
+      textAlign: "center",
+      borderColor: "grey",
+      // iOS Shadow
+      shadowColor: "#000",
+      shadowOffset: {
+        width: 0,
+        height: 2,
+      },
+      shadowOpacity: 0.25,
+      shadowRadius: 3.84,
+      // Android Shadow
+      elevation: 5,
+      borderRadius: 10,
+      padding: 10,
+      backgroundColor: "#f8f8f8",
+    },
+  });
 
   return (
     <View style={{ flex: 1, backgroundColor: "white" }}>
       <Text className="text-center text-3xl m-5">Категории на продукти</Text>
       <ScrollView
         contentContainerStyle={{
-          flexDirection: "row",
+          flexDirection: "column",
           flexWrap: "wrap",
-          justifyContent: "space-evenly",
+          marginHorizontal: "auto",
+          width: "90%",
         }}
       >
         {categories.map((item) => (
           <Link href={"category/" + item.id} asChild key={item.id}>
             <Pressable style={styles.categoryContainer} className="w-full">
+              <View
+                style={{
+                  width: 0.54 * screenWidth,
+                }}
+              >
+                <Text className="text-3xl font-medium">{item.name}</Text>
+                <Text className="text-xl">
+                  This is an example descri ptiosdn asdj aowijdeawoij whe
+                </Text>
+              </View>
               <Image
                 source={require(`../../assets/category/4.jpg`)}
                 style={{
-                  height: screenWidth * 0.35,
-                  width: screenWidth * 0.35,
+                  //   position: "absolute",
+                  //   right: screenWidth * 0.03,
+                  height: screenWidth * 0.3,
+                  width: screenWidth * 0.3,
                   borderRadius: 8,
                 }}
               />
-              <Text className="text-xl">{item.name}</Text>
             </Pressable>
           </Link>
         ))}
@@ -53,27 +89,3 @@ export default function ProductsScreen() {
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  categoryContainer: {
-    alignItems: "center",
-    margin: 5,
-    borderWidth: 1,
-    maxWidth: "40%",
-    textAlign: "center",
-    borderColor: "grey",
-    // iOS Shadow
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-    // Android Shadow
-    elevation: 5,
-    borderRadius: 10,
-    padding: 10,
-    backgroundColor: "#f8f8f8",
-  },
-});
