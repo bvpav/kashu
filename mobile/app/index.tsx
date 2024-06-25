59ff2f4dc727cd3bf945bf0d3e2173dd1c675ce0
Iva Marinova <ivamarinova06@gmail.com>
2024-06-25 20:24:32 +0300

    Implemented basic ar with viro

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,29 +1,5 @@
-import { Link, Stack } from "expo-router";
-import { StyleSheet, Text, View } from "react-native";
+// index.tsx
+import { registerRootComponent } from 'expo';
+import MyARScene from '../components/MyARScene';
 
-export default function IndexScreen() {
-  return (
-    <>
-      <Stack.Screen options={{ title: "Home screen" }} />
-      <View style={styles.container} className="bg-slate-100">
-        <Text className="text-black">This screen is the index screen.</Text>
-        <Link href="/test" className="bg-gray-400" style={styles.link}>
-          <Text>Go to test screen!</Text>
-        </Link>
-      </View>
-    </>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    padding: 20,
-  },
-  link: {
-    marginTop: 15,
-    paddingVertical: 15,
-  },
-});
+registerRootComponent(MyARScene);
