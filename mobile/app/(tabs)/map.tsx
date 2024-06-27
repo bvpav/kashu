7ec8a7c19717a81a6f500d423b79b9a1fd9812d6
bvpav <78971592+bvpav@users.noreply.github.com>
2024-06-27 15:28:20 +0300

    Added AR toggle

--- mobile/app/(tabs)/map.tsx
+++ mobile/app/(tabs)/map.tsx
@@ -1,10 +1,47 @@
-import { useRef } from "react";
-import { Button, View } from "react-native";
-import { RNLeaflet } from "@/components/leaflet/Leaflet";
-import type { RNLeafletRef } from "@/components/leaflet/types";
+import React from "react";
+import { TouchableOpacity, View, StyleSheet, Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import StoreMap from "@/components/store-map/StoreMap";
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
 
 export default function Map() {
-  return <StoreMap />;
+  const [isARView, setIsARView] = React.useState(false);
+
+  return (
+    <SafeAreaView style={styles.container}>
+      {!isARView && <StoreMap />}
+      <TouchableOpacity
+        style={styles.buttonContainer}
+        onPress={() => setIsARView((prev) => !prev)}
+      >
+        <Text style={styles.buttonText}>
+          <MaterialCommunityIcons name="skull-scan" size={24} />
+        </Text>
+      </TouchableOpacity>
+    </SafeAreaView>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  buttonContainer: {
+    position: "absolute",
+    top: 20,
+    right: 20,
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    backgroundColor: "blue", // You can change the color as needed
+    justifyContent: "center",
+    alignItems: "center",
+    zIndex: 1000,
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 24,
+    lineHeight: 24,
+    fontWeight: "bold",
+  },
+});
