3d05084c44c44edb7ccad7a089f97b0b7291b48f
bvpav <78971592+bvpav@users.noreply.github.com>
2024-06-27 12:36:47 +0300

    tried another hardcoded path

--- mobile/hooks/useStorePath.ts
+++ mobile/hooks/useStorePath.ts
@@ -36,12 +36,27 @@ const path = [
   {
     is_collectable: true,
     x: 3,
-    y: 3,
+    y: 4,
   },
   {
     is_collectable: false,
     x: 2,
+    y: 3,
+  },
+  {
+    is_collectable: true,
+    x: 3,
     y: 2,
+  },
+  {
+    is_collectable: false,
+    x: 2,
+    y: 1,
+  },
+  {
+    is_collectable: true,
+    x: 2,
+    y: 0,
   },
   {
     is_collectable: false,
@@ -59,26 +74,6 @@ const path = [
     y: 2,
   },
   {
-    is_collectable: true,
-    x: 4,
-    y: 3,
-  },
-  {
-    is_collectable: false,
-    x: 5,
-    y: 3,
-  },
-  {
-    is_collectable: true,
-    x: 6,
-    y: 3,
-  },
-  {
-    is_collectable: false,
-    x: 5,
-    y: 2,
-  },
-  {
     is_collectable: false,
     x: 6,
     y: 1,
@@ -92,41 +87,71 @@ const path = [
     is_collectable: false,
     x: 8,
     y: 2,
-  },
-  {
-    is_collectable: true,
-    x: 7,
-    y: 3,
-  },
-  {
-    is_collectable: false,
-    x: 8,
-    y: 4,
   },
   {
     is_collectable: false,
     x: 9,
-    y: 5,
+    y: 1,
   },
   {
     is_collectable: false,
     x: 10,
-    y: 5,
+    y: 1,
   },
   {
     is_collectable: false,
     x: 11,
+    y: 2,
+  },
+  {
+    is_collectable: true,
+    x: 12,
+    y: 2,
+  },
+  {
+    is_collectable: true,
+    x: 13,
+    y: 3,
+  },
+  {
+    is_collectable: true,
+    x: 13,
     y: 4,
   },
   {
-    is_collectable: false,
+    is_collectable: true,
+    x: 13,
+    y: 3,
+  },
+  {
+    is_collectable: true,
     x: 12,
-    y: 5,
+    y: 2,
   },
   {
     is_collectable: false,
     x: 13,
-    y: 5,
+    y: 1,
+  },
+  {
+    is_collectable: false,
+    x: 14,
+    y: 2,
+  },
+  {
+    is_collectable: true,
+    x: 13,
+    y: 3,
+  },
+  {
+    is_collectable: false,
+    x: 14,
+    y: 3,
+  },
+  {
+    is_collectable: true,
+    x: 15,
+    y: 3,
   },
   {
     is_collectable: false,
@@ -141,257 +166,102 @@ const path = [
   {
     is_collectable: false,
     x: 16,
-    y: 5,
-  },
-  {
-    is_collectable: false,
-    x: 17,
-    y: 4,
-  },
-  {
-    is_collectable: false,
-    x: 18,
-    y: 5,
-  },
-  {
-    is_collectable: false,
-    x: 19,
-    y: 5,
-  },
-  {
-    is_collectable: false,
-    x: 20,
-    y: 4,
-  },
-  {
-    is_collectable: false,
-    x: 21,
-    y: 5,
-  },
-  {
-    is_collectable: false,
-    x: 22,
-    y: 5,
-  },
-  {
-    is_collectable: false,
-    x: 23,
-    y: 4,
-  },
-  {
-    is_collectable: false,
-    x: 24,
-    y: 5,
-  },
-  {
-    is_collectable: false,
-    x: 25,
-    y: 5,
-  },
-  {
-    is_collectable: false,
-    x: 26,
-    y: 4,
-  },
-  {
-    is_collectable: true,
-    x: 27,
-    y: 4,
-  },
-  {
-    is_collectable: false,
-    x: 28,
-    y: 5,
-  },
-  {
-    is_collectable: false,
-    x: 29,
     y: 6,
   },
   {
     is_collectable: false,
-    x: 30,
+    x: 17,
     y: 7,
-  },
-  {
-    is_collectable: false,
-    x: 31,
-    y: 7,
-  },
-  {
-    is_collectable: false,
-    x: 32,
-    y: 7,
-  },
-  {
-    is_collectable: false,
-    x: 33,
-    y: 7,
-  },
-  {
-    is_collectable: false,
-    x: 34,
-    y: 7,
-  },
-  {
-    is_collectable: true,
-    x: 35,
-    y: 8,
-  },
-  {
-    is_collectable: false,
-    x: 36,
-    y: 7,
-  },
-  {
-    is_collectable: false,
-    x: 37,
-    y: 7,
-  },
-  {
-    is_collectable: false,
-    x: 38,
-    y: 8,
-  },
-  {
-    is_collectable: false,
-    x: 38,
-    y: 9,
-  },
-  {
-    is_collectable: false,
-    x: 37,
-    y: 10,
-  },
-  {
-    is_collectable: false,
-    x: 36,
-    y: 10,
-  },
-  {
-    is_collectable: true,
-    x: 35,
-    y: 9,
-  },
-  {
-    is_collectable: false,
-    x: 34,
-    y: 10,
-  },
-  {
-    is_collectable: false,
-    x: 33,
-    y: 10,
-  },
-  {
-    is_collectable: false,
-    x: 32,
-    y: 10,
-  },
-  {
-    is_collectable: false,
-    x: 31,
-    y: 10,
-  },
-  {
-    is_collectable: false,
-    x: 30,
-    y: 10,
-  },
-  {
-    is_collectable: false,
-    x: 29,
-    y: 10,
-  },
-  {
-    is_collectable: false,
-    x: 28,
-    y: 10,
-  },
-  {
-    is_collectable: false,
-    x: 27,
-    y: 10,
-  },
-  {
-    is_collectable: false,
-    x: 26,
-    y: 11,
-  },
-  {
-    is_collectable: false,
-    x: 25,
-    y: 12,
-  },
-  {
-    is_collectable: false,
-    x: 24,
-    y: 13,
-  },
-  {
-    is_collectable: false,
-    x: 23,
-    y: 14,
-  },
-  {
-    is_collectable: false,
-    x: 22,
-    y: 15,
-  },
-  {
-    is_collectable: false,
-    x: 21,
-    y: 15,
-  },
-  {
-    is_collectable: false,
-    x: 20,
-    y: 16,
-  },
-  {
-    is_collectable: false,
-    x: 19,
-    y: 16,
   },
   {
     is_collectable: false,
     x: 18,
-    y: 15,
+    y: 7,
+  },
+  {
+    is_collectable: false,
+    x: 19,
+    y: 7,
+  },
+  {
+    is_collectable: false,
+    x: 20,
+    y: 7,
+  },
+  {
+    is_collectable: false,
+    x: 21,
+    y: 8,
+  },
+  {
+    is_collectable: false,
+    x: 22,
+    y: 9,
+  },
+  {
+    is_collectable: true,
+    x: 23,
+    y: 10,
+  },
+  {
+    is_collectable: false,
+    x: 22,
+    y: 10,
+  },
+  {
+    is_collectable: false,
+    x: 21,
+    y: 10,
+  },
+  {
+    is_collectable: false,
+    x: 20,
+    y: 10,
+  },
+  {
+    is_collectable: false,
+    x: 19,
+    y: 10,
+  },
+  {
+    is_collectable: false,
+    x: 18,
+    y: 10,
   },
   {
     is_collectable: false,
     x: 17,
-    y: 15,
+    y: 10,
   },
   {
     is_collectable: false,
     x: 16,
-    y: 16,
+    y: 10,
   },
   {
     is_collectable: false,
     x: 15,
-    y: 16,
+    y: 10,
   },
   {
     is_collectable: false,
     x: 14,
-    y: 15,
+    y: 11,
   },
   {
     is_collectable: false,
     x: 13,
-    y: 15,
+    y: 12,
   },
   {
     is_collectable: false,
     x: 12,
-    y: 16,
+    y: 13,
   },
   {
     is_collectable: false,
     x: 11,
-    y: 16,
+    y: 14,
   },
   {
     is_collectable: false,
@@ -416,12 +286,12 @@ const path = [
   {
     is_collectable: false,
     x: 6,
-    y: 17,
+    y: 18,
   },
   {
-    is_collectable: false,
+    is_collectable: true,
     x: 5,
-    y: 17,
+    y: 18,
   },
   {
     is_collectable: false,
@@ -444,7 +314,7 @@ const path = [
     y: 17,
   },
   {
-    is_collectable: false,
+    is_collectable: true,
     x: 0,
     y: 17,
   },
