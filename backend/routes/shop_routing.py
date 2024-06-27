c910376c608e179d7710cf2033e5a3f0d73dd792
Kerzoken <kerzo.gm@gmail.com>
2024-06-28 02:38:44 +0300

    Optimized routing algorithm and cleaned code

--- backend/routes/shop_routing.py
+++ backend/routes/shop_routing.py
@@ -2,7 +2,6 @@ import heapq
 
 
 def print_grid(map):
-    """Helper function to print the grid"""
     for i in range(9, -1, -1):
         for j in range(10):
             print(map[i][j], end=' ')
@@ -10,8 +9,7 @@ def print_grid(map):
 
 
 def is_valid_move(x, y, grid, just_collected=False):
-    """Helper function to check if a move is valid"""
-    if 0 <= x < len(grid) and 0 <= y < len(grid[0]) and grid[x][y] != 'X' and grid[x][y] != 'B' and grid[x][y] != 'A':
+    if 0 <= x < len(grid) and 0 <= y < len(grid[0]) and grid[x][y] not in ('X', 'B', 'A'):
         if just_collected and grid[x][y] == '1':
             return False
         return True
@@ -19,22 +17,17 @@ def is_valid_move(x, y, grid, just_collected=False):
 
 
 def heuristic(a, b):
-    """Heuristic function for A* (Manhattan Distance)"""
     return abs(a[0] - b[0]) + abs(a[1] - b[1])
 
 
-def is_adjacent(a, b):
-    return abs(a[0] - b[0]) <= 1 and abs(a[1] - b[1]) <= 1
-
-
 def a_star(start, goal, grid, just_collected=False):
-    """A* Pathfinding Algorithm"""
     open_set = []
     heapq.heappush(open_set, (0, start))
     came_from = {}
     g_score = {start: 0}
     f_score = {start: heuristic(start, goal)}
     first_move_after_collect = just_collected
+
     while open_set:
         _, current = heapq.heappop(open_set)
         if current == goal:
@@ -45,18 +38,14 @@ def a_star(start, goal, grid, just_collected=False):
             path.append(start)
             path.reverse()
             return path
+
         neighbors = [(0, 1), (1, 0), (0, -1), (-1, 0),
                      (1, 1), (1, -1), (-1, 1), (-1, -1)]
         for direction in neighbors:
             neighbor = (current[0] + direction[0], current[1] + direction[1])
             if is_valid_move(neighbor[0], neighbor[1], grid, first_move_after_collect):
-                # Skip if the neighbor is the goal and the current is the start
-                if start == (12, 11):
-                    print("YOYOYOYOOY: ", current, goal,
-                          start, grid[current[0]][current[1]], neighbor)
-                if (grid[neighbor[0]][neighbor[1]] == '1' and current == start) or (grid[neighbor[0]][neighbor[1]] == '1' and grid[current[0]][current[1]] != '.'):
-                    print("NEIGHBOR: ", neighbor, current,
-                          goal, grid[current[0]][current[1]])
+                if (grid[neighbor[0]][neighbor[1]] == '1' and current == start) or \
+                   (grid[neighbor[0]][neighbor[1]] == '1' and grid[current[0]][current[1]] != '.'):
                     continue
                 tentative_g_score = g_score[current] + 1
                 if neighbor not in g_score or tentative_g_score < g_score[neighbor]:
@@ -68,63 +57,38 @@ def a_star(start, goal, grid, just_collected=False):
                         f_score[neighbor] -= 10  # prioritize empty spaces
                     heapq.heappush(open_set, (f_score[neighbor], neighbor))
         first_move_after_collect = False  # Reset the flag after the first move
+
     return []
 
 
 def collect_items(start, items, grid):
     path = []
     current_position = start
-    items = list(sorted(items, key=lambda item: heuristic(start, item)))
+    items = sorted(items, key=lambda item: heuristic(start, item))
 
-    collected_items = []
+    collected_items = set()
 
     for item in items:
         if item in collected_items:
             continue
 
-        path_segment = None
-
-        print(grid[current_position[0]][current_position[1]])
-
-        # if grid[current_position[0]][current_position[1]] == '#':
-        #     grid[current_position[0]][current_position[1]] = '1'
-        #     path_segment = a_star(current_position, item,
-        #                           grid, just_collected=bool(path))
-        #     grid[current_position[0]][current_position[1]] = '#'
-        # else:
         path_segment = a_star(current_position, item,
                               grid, just_collected=bool(path))
 
-        print("PATH SEGMENT: ", path_segment)
-        # fill arr with path values
+        if not path_segment:
+            return []
 
         for p in path_segment:
             if p in items or grid[p[0]][p[1]] == '1':
-                #         grid[p[0]][p[1]] = '#'
-                collected_items.append(p)
-                print("COLLECTED: ", p)
-                print(grid[p[0]][p[1]])
-
-        arr = []
-        for a in path_segment:
-            arr.append(grid[a[0]][a[1]])
-
-        print("ARR: ", arr)
+                collected_items.add(p)
 
         if path_segment:
             # Exclude the last position to avoid repetition
-            path.extend(path_segment)
+            path.extend(path_segment[:-1])
             path.append(item)  # Collect the item
             current_position = item
-        else:
-            return []
+
     return path
-
-
-# Define the start, end, and items positions
-start = (0, 0)
-end = (9, 9)
-items = [(2, 3), (4, 4), (4, 7), (2, 4)]
 
 
 def get_path(start, end, items, arr):
@@ -145,11 +109,7 @@ def get_path(start, end, items, arr):
         if full_path[i] != full_path[i - 1]:
             cleaned_path.append(full_path[i])
 
-    # # Print the path
-    # print("Path:")
-    # for step in cleaned_path:
-    #     print(step)
-
+    # Add a flag indicating if the position is an item
     cleaned_path = [(x[0], x[1], x in items) for x in cleaned_path]
 
     return cleaned_path
--- backend/routes/shopping_list.py
+++ backend/routes/shopping_list.py
@@ -3,37 +3,29 @@ from flask import Blueprint, request, jsonify
 from backend.models import db, Product, Location
 from backend.routes.shop_routing import get_path
 from backend.map_generation import generate_map, get_exit, get_start, get_checkouts
-from sqlalchemy import join
 
 shopping_list_bp = Blueprint('shopping_list', __name__)
 
 
 @shopping_list_bp.route('/api/shopping-list', methods=['POST'])
 def get_shopping_list_route():
-
-    golden_eggs_pos = ((4, 27), (11, 32), (10, 23), (11, 22), (20, 25))
+    golden_eggs_pos = [(4, 27), (11, 32), (10, 23), (11, 22), (20, 25)]
 
     ids = request.json.get('ids', [])
     products = Product.query.filter(Product.id.in_(ids)).all()
-
     if not products:
         return jsonify({'error': 'No products found'}), 404
 
-    print("PRODUCTS", products)
-    for product in products:
-        print(product.id)
-
     locations = db.session.query(Location).join(
-        Product, Location.location_id == Product.product_id).filter(Product.id.in_(ids)).all()
+        Product, Location.location_id == Product.product_id
+    ).filter(Product.id.in_(ids)).all()
 
     items = [(location.y, location.x) for location in locations]
-
     map = generate_map()
     start_time = time.time()
 
     start = get_start()
     end = get_exit()
-
     map[end[0]][end[1]] = '.'
     map[start[0]][start[1]] = '.'
 
@@ -42,68 +34,58 @@ def get_shopping_list_route():
             if (i, j) not in items and map[i][j] != '.':
                 map[i][j] = 'X'
 
-    # make all the items as 1
     for item in items:
         map[item[0]][item[1]] = '1'
 
-    paths = []
-
     checkouts = get_checkouts()
+    paths = []
+    golden_eggs_in_items = any(item in golden_eggs_pos for item in items)
 
-    # check if golden eggs are in items
-    golden_eggs = False
-    for item in items:
-        if item in golden_eggs_pos:
-            golden_eggs = True
-            break
-
-    if not golden_eggs:
+    if not golden_eggs_in_items:
         for checkout in checkouts:
-            # items.append(checkout)
-            current_end = checkout
             map[checkout[0]][checkout[1]] = '1'
-            print("CURRENT_END", current_end)
-
             for golden_egg in golden_eggs_pos:
                 items.append(golden_egg)
                 map[golden_egg[0]][golden_egg[1]] = '1'
-                print("ITEMS", items)
-                paths.append(get_path(start, current_end, items, map))
+                paths.append(get_path(start, checkout, items, map))
                 map[golden_egg[0]][golden_egg[1]] = 'X'
                 items.pop()
-
             map[checkout[0]][checkout[1]] = 'X'
-            # items.pop()
     else:
         for checkout in checkouts:
-            # items.append(checkout)
-            current_end = checkout
             map[checkout[0]][checkout[1]] = '1'
-            print("CURRENT_END", current_end)
-
-            paths.append(get_path(start, current_end, items, map))
+            paths.append(get_path(start, checkout, items, map))
             map[checkout[0]][checkout[1]] = 'X'
 
-    shortest_path = min(paths, key=lambda x: len(x))
+    if not paths:
+        return jsonify({'error': 'No path found to collect all items'}), 404
 
+    shortest_path = min(paths, key=len)
     map[end[0]][end[1]] = '1'
     shortest_path[-1] = (shortest_path[-1][0], shortest_path[-1][1], True)
-
     shortest_path.extend(get_path(shortest_path[-1], end, [end], map)[1:])
 
-    print(shortest_path)
+    cleaned_path = [shortest_path[0]]
+    for i in range(1, len(shortest_path)):
+        if shortest_path[i] != shortest_path[i - 1]:
+            cleaned_path.append(shortest_path[i])
 
-    shortest_path = [{'y': path[0], 'x': path[1],
-                      'is_collectable': path[2]} for path in shortest_path]
-
-    # set golden eggs as not collectable
-    # for golden_egg in golden_eggs_pos:
-    #     for path in shortest_path:
-    #         if path['y'] == golden_egg[0] and path['x'] == golden_egg[1]:
-    #             path['is_collectable'] = False
+    cleaned_path = [{'y': path[0], 'x': path[1],
+                     'is_collectable': path[2]} for path in cleaned_path]
 
     end_time = time.time()
     execution_time = end_time - start_time
     print(f"Execution Time: {execution_time} seconds")
 
-    return jsonify(shortest_path)
+    for path in cleaned_path:
+        if path['is_collectable']:
+            location_id = db.session.query(Location.location_id).filter(
+                Location.x == path['x'], Location.y == path['y']
+            ).first()
+            if location_id:
+                product = Product.query.filter(
+                    Product.product_id == location_id[0]).first()
+                if product:
+                    path['product_name'] = product.name
+
+    return jsonify(cleaned_path)
